18af82183395f593e35354ca0e86a061ac5d31e8
moonkhan12 <82519898+moonkhan12@users.noreply.github.com>
2021-04-14 09:38:44 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# EcommerceWebsite
+ 
+This is the code for the software engineering project
+
+That is all
+
+
+
+
+
+
+
+
+
---
+++ js/login.js
@@ -0,0 +1,19 @@
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
